e1d42e188836a5b069b50f903a40dddcb5ccbb7d
heimgun <heidi.guneriussen@live.no>
2019-11-06 11:34:30 +0100

    Updated Main Menu GUI

--- src/com/example/DicePoker/GUI/MainGUI.java
+++ src/com/example/DicePoker/GUI/MainGUI.java
@@ -1,13 +1,17 @@
 package com.example.DicePoker.GUI;
 
+import com.sun.java.accessibility.util.TopLevelWindowListener;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MainGUI extends Application {
@@ -26,39 +30,64 @@ public class MainGUI extends Application {
         // Use a FlowPane for the root node. In this case,
         // vertical and horizontal gaps of 10.
         FlowPane rootNode = new FlowPane(10, 10);
+        GridPane mainMenuGrid = new GridPane();
+        mainMenuGrid.setVgap(10);
+
 
         // Center the controls in the scene.
         rootNode.setAlignment(Pos.CENTER);
 
         // Create a scene.
-        Scene myScene = new Scene(rootNode, 300, 100);
+        Scene myScene = new Scene(rootNode, 200, 300);
 
         // Set the scene on the stage.
         myStage.setScene(myScene);
 
         // Create a label.
-        response = new Label("Push a Button");
+        response = new Label("DicePoker");
+
 
         // Create two push buttons.
-        Button btnUp = new Button("Up");
-        Button btnDown = new Button("Down");
+        Button btnNewGame = new Button("New Game");
+        //TODO -- Make load button -- //
+        Button btnRules = new Button("Rules");
+        Button btnQuit = new Button("Quit");
 
-        // Handle the action events for the Up button.
-        btnUp.setOnAction(new EventHandler<ActionEvent>() {
+        btnQuit.centerShapeProperty();
+
+        mainMenuGrid.add(response, 0,1);
+        mainMenuGrid.add(btnNewGame,0,2);
+        mainMenuGrid.add(btnRules,0,3);
+        mainMenuGrid.add(btnQuit,0,4);
+
+        // Handle the action events for the New Game Button
+        btnNewGame.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent ae) {
-                response.setText("You pressed Up.");
+                response.setText("You pressed New Game.");
             }
+            //TODO -- Sett action til ny scene -- //
+
         });
 
-        // Handle the action events for the Down button.
-        btnDown.setOnAction(new EventHandler<ActionEvent>() {
+        // Handle the action events for the Rules button.
+        btnRules.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent ae) {
                 response.setText("You pressed Down.");
             }
+            //TODO -- Sett action til ny scene --//
+
+        });
+
+        // Handle the action events for the New Game Button
+        btnQuit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent ae) {
+                System.exit(0);
+            }
+
         });
 
         // Add the label and buttons to the scene graph.
-        rootNode.getChildren().addAll(btnUp, btnDown, response);
+        rootNode.getChildren().addAll(mainMenuGrid);
 
         // Show the stage and its scene.
         myStage.show();
