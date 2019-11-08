dac6880e9e5930a504fd2eb745ed366793140d70
SebastianLindfors <sebastianlindfors@gmail.com>
2019-11-08 16:20:00 +0100

    Removed all files that are in the .gitignore

--- src/com/example/DicePoker/Logic/Controller.java
+++ src/com/example/DicePoker/Logic/Controller.java
@@ -1,9 +1,91 @@
 package com.example.DicePoker.Logic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Controller {
-    Player player1 = new Player("Kamil",10);
-    Player player2 = new Player("Nusret",10);
-    Player player3 = new Player("Heidi",10);
-    Player player4 = new Player("Sebastian",10);
+
+    ArrayList<Player> playersInGame = new ArrayList<>(); //Stores all players in the game.
+
+    int currentPlayer;
+    int firstPlayer;
+
+    public Controller(ArrayList<Player> playersInGame) {
+
+        this.playersInGame = playersInGame;
+
+    }
+
+    public static int[] determineHandStrength (int[] diceHand) {
+        /**
+         * 0 = Nothing
+         * 1 = Pair
+         * 2 = Two Pairs
+         * 3 = Three of a Kind
+         * 4 = Straight
+         * 5 = Full House
+         * 6 = Four of a Kind
+         * 7 = Five of a Kind
+         *
+         */
+
+        Map<Integer, Integer>  dieValues= new HashMap<Integer, Integer>();
+
+        dieValues.put(1, 0);
+        dieValues.put(2, 0);
+        dieValues.put(3, 0);
+        dieValues.put(4, 0);
+        dieValues.put(5, 0);
+        dieValues.put(6, 0);
+
+        int mostRolled = 0;
+        int mostRolledValue = -1;
+
+        for (int value : diceHand) {
+            dieValues.put(value, (dieValues.get(value) + 1));
+            if (dieValues.get(value) > mostRolled) {
+                mostRolled++;
+                mostRolledValue = value;
+            }
+        }
+        int[] output = new int[4];
+        switch (mostRolled) {
+            case 5: //This means person has a Five of a Kind hand
+                    output[0] = 7;
+                    output [1] = mostRolledValue;
+                    break;
+            case 4: //This means the player has a Four of a kind.
+                    output[0] = 6;
+                    output[1] = mostRolledValue;
+
+                    break;
+            case 3: // This means the player has either Three of a Kind of a Full House.
+                break;
+            case 2:  //This means the player has a pair or two pairs.
+                break;
+            case 1: //This means the player has nothing or a straight.
+                break;
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+//    Player player1 = new Player("Kamil",10, true);
+//    Player player2 = new Player("Nusret",10, true );
+//    Player player3 = new Player("Heidi",10,true );
+//    Player player4 = new Player("Sebastian",10, true);
+
+
+
+
 
 }
--- src/com/example/DicePoker/Testing/ControllerTest.java
+++ src/com/example/DicePoker/Testing/ControllerTest.java
@@ -1,5 +1,23 @@
-import static org.junit.Assert.*;
+package com.example.DicePoker.Testing;
+
+import com.example.DicePoker.Logic.Controller;
+import org.junit.Assert;
+import org.junit.Test;
+
+
 
 public class ControllerTest {
+
+    @Test
+    public void determineHandStrengthTest() {
+
+        int[] inputHand = {1,2,3,4,5};
+        int[] expected = {1,1,1,1,1,0};
+        Assert.assertArrayEquals( expected, Controller.determineHandStrength(inputHand));
+
+        inputHand = new int[]{1,1,1,1,1};
+        expected = new int[]{5,0,0,0,0,0};
+        Assert.assertArrayEquals( expected, Controller.determineHandStrength(inputHand));
+}
 
 }
