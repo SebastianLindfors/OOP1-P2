14fc49c6d25884f6aac10bb82a23af453b052fab
SebastianLindfors <sebastianlindfors@gmail.com>
2019-11-09 00:25:00 +0100

    Completed hansStrengthComparason method and testing

--- src/com/example/DicePoker/Logic/Controller.java
+++ src/com/example/DicePoker/Logic/Controller.java
@@ -17,6 +17,8 @@ public class Controller {
 
     }
 
+
+
     public static int[] determineHandStrength (int[] diceHand) {
         /**
          * 0 = Nothing
@@ -49,7 +51,8 @@ public class Controller {
                 mostRolledValue = value;
             }
         }
-        int[] output = new int[4];
+
+        int[] output = new int[5];
         switch (mostRolled) {
             case 5: //This means person has a Five of a Kind hand
                     output[0] = 7;
@@ -58,23 +61,165 @@ public class Controller {
             case 4: //This means the player has a Four of a kind.
                     output[0] = 6;
                     output[1] = mostRolledValue;
-
+                    for (int value : dieValues.keySet()) {
+                        if (dieValues.get(value) == 1) {
+                            output[2] = value;
+                            break;
+                        }
+                    }
                     break;
             case 3: // This means the player has either Three of a Kind of a Full House.
+                int tiebreaker1 = -1;
+                for (int value : dieValues.keySet()) {
+                    if (dieValues.get(value) == 2) { // This means the player has a full house.
+                        output[0] = 5;
+                        output[1] = mostRolledValue;
+                        output[2] = value;
+                        break;
+                    }
+                    else if (dieValues.get(value) == 1){
+                        if (tiebreaker1 == -1) {
+                            tiebreaker1 = value;
+                        }
+                        else {
+                            if (value > tiebreaker1) { // This means the player has three of a kind
+                                output[0] = 3;
+                                output[1] = mostRolledValue;
+                                output[2] = value;
+                                output[3] = tiebreaker1;
+                                break;
+                            }
+                            else {
+                                output[0] = 3;
+                                output[1] = mostRolledValue;
+                                output[2] = tiebreaker1;
+                                output[3] = value;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
                 break;
             case 2:  //This means the player has a pair or two pairs.
+                int secondPair = -1;
+
+                int kicker1 = -1;
+                int kicker2 = -1;
+
+                for (int value : dieValues.keySet()) {
+                    if (dieValues.get(value) == 2 && value != mostRolledValue) { // This means the player has 2 pairs
+                        secondPair = value;
+                    }
+                    else if (dieValues.get(value) == 1) {
+                        if (kicker1 == -1) {
+                            kicker1 = value;
+                        }
+                        else if (kicker2 == -1) {
+                            kicker2 = value;
+                        }
+                        else {
+                            if (kicker1 > kicker2) {
+                                if (kicker2 > value) {
+                                    output[0] = 1;
+                                    output[1] = mostRolledValue;
+                                    output[2] = kicker1;
+                                    output[3] = kicker2;
+                                    output[4] = value;
+                                    break;
+                                }
+                                else {
+                                    if (kicker1 > value) {
+                                        output[0] = 1;
+                                        output[1] = mostRolledValue;
+                                        output[2] = kicker1;
+                                        output[3] = value;
+                                        output[4] = kicker2;
+                                        break;
+                                    }
+                                    else {
+                                        output[0] = 1;
+                                        output[1] = mostRolledValue;
+                                        output[2] = value;
+                                        output[3] = kicker1;
+                                        output[4] = kicker2;
+                                        break;
+                                    }
+                                }
+                            }
+                            else if (kicker2 > value) {
+                                if (kicker1 > value) {
+                                    output[0] = 1;
+                                    output[1] = mostRolledValue;
+                                    output[2] = kicker2;
+                                    output[3] = kicker1;
+                                    output[4] = value;
+                                    break;
+                                }
+                                else {
+                                    output[0] = 1;
+                                    output[1] = mostRolledValue;
+                                    output[2] = kicker2;
+                                    output[3] = value;
+                                    output[4] = kicker1;
+                                    break;
+                                }
+                            }
+                            else {
+                                output[0] = 1;
+                                output[1] = mostRolledValue;
+                                output[2] = value;
+                                output[3] = kicker2;
+                                output[4] = kicker1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (output[0] == 0) {
+                    if (secondPair > mostRolledValue) {
+                        output[0] = 2;
+                        output[1] = secondPair;
+                        output[2] = mostRolledValue;
+                        output[3] = kicker1;
+                    }
+                    else {
+                        output[0] = 2;
+                        output[1] = mostRolledValue;
+                        output[2] = secondPair;
+                        output[3] = kicker1;
+                    }
+                }
                 break;
             case 1: //This means the player has nothing or a straight.
+                if (dieValues.get(1) == 0) {
+                    output[0] = 4;
+                    output[1] = 6;
+                }
+                else if (dieValues.get(6) == 0) {
+                    output[0] = 4;
+                    output[1] = 5;
+                }
+                else if (dieValues.get(2) == 0) {
+                    output[0] = 0;
+                    output[1] = 2;
+                }
+                else if (dieValues.get(3) == 0) {
+                    output[0] = 0;
+                    output[1] = 3;
+                }
+                else if (dieValues.get(4) == 0) {
+                    output[0] = 0;
+                    output[1] = 4;
+                }
+                else if (dieValues.get(5) == 0) {
+                    output[0] = 0;
+                    output[1] = 5;
+                }
                 break;
-
         }
-
-
-
-
-
-
-
+        return output;
     }
 
 
--- src/com/example/DicePoker/Testing/ControllerTest.java
+++ src/com/example/DicePoker/Testing/ControllerTest.java
@@ -11,13 +11,151 @@ public class ControllerTest {
     @Test
     public void determineHandStrengthTest() {
 
-        int[] inputHand = {1,2,3,4,5};
-        int[] expected = {1,1,1,1,1,0};
-        Assert.assertArrayEquals( expected, Controller.determineHandStrength(inputHand));
+        //Pair test 1
+        int[] inputHand = {1,1,3,4,5};
+        int[] expected  = {1,1,5,4,3};
+        int[] actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
 
-        inputHand = new int[]{1,1,1,1,1};
-        expected = new int[]{5,0,0,0,0,0};
-        Assert.assertArrayEquals( expected, Controller.determineHandStrength(inputHand));
+        //Pair test 2.
+        inputHand   = new int[]{2,2,3,4,5};
+        expected    = new int[]{1,2,5,4,3};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Pair test 3.
+        inputHand   = new int[]{5,5,1,4,6};
+        expected    = new int[]{1,5,6,4,1};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Two Pair test 1.
+        inputHand   = new int[]{5,5,4,4,6};
+        expected    = new int[]{2,5,4,6,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Two Pair test 2.
+        inputHand   = new int[]{1,1,2,2,6};
+        expected    = new int[]{2,2,1,6,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Two Pair test 3.
+        inputHand   = new int[]{1,1,3,3,2};
+        expected    = new int[]{2,3,1,2,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Three of a kind test 1.
+        inputHand   = new int[]{1,1,1,3,2};
+        expected    = new int[]{3,1,3,2,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Three of a kind test 2.
+        inputHand   = new int[]{6,6,6,3,1};
+        expected    = new int[]{3,6,3,1,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Straight test 1.
+        inputHand   = new int[]{1,2,3,4,5};
+        expected    = new int[]{4,5,0,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Straight test 2.
+        inputHand   = new int[]{2,3,4,5,6};
+        expected    = new int[]{4,6,0,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Full House test 1.
+        inputHand   = new int[]{2,2,2,5,5};
+        expected    = new int[]{5,2,5,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Full House 2.
+        inputHand   = new int[]{6,6,6,5,5};
+        expected    = new int[]{5,6,5,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Full House 3.
+        inputHand   = new int[]{3,3,3,4,4};
+        expected    = new int[]{5,3,4,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Four of a Kind Test 1.
+        inputHand   = new int[]{6,6,6,6,1};
+        expected    = new int[]{6,6,1,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Four of a Kind Test 2.
+        inputHand   = new int[]{4,4,3,4,4};
+        expected    = new int[]{6,4,3,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Four of a Kind Test 3.
+        inputHand   = new int[]{3,3,3,3,4};
+        expected    = new int[]{6,3,4,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Five of a kind test 1.
+        inputHand   = new int[]{1,1,1,1,1};
+        expected    = new int[]{7,1,0,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Five of a kind test 2.
+        inputHand   = new int[]{5,5,5,5,5};
+        expected    = new int[]{7,5,0,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
+
+        //Five of a kind test 3.
+        inputHand   = new int[]{3,3,3,3,3};
+        expected    = new int[]{7,3,0,0,0};
+        actual = Controller.determineHandStrength(inputHand);
+        System.out.println(handStrengthOutput(expected, actual));
+        Assert.assertArrayEquals( expected, actual);
 }
+
+private String handStrengthOutput(int[] expected, int [] actual) {
+        StringBuilder string1 = new StringBuilder();
+        StringBuilder string2 = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            string1.append(expected[i]);
+            string1.append(" ");
+            string2.append(actual[i]);
+            string2.append(" ");
+        }
+
+        return ("Expected: " + string1.toString() + " Actual: " +string2.toString());
+    }
 
 }
